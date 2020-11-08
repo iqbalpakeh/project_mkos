362e2f5dd6f323f6be77c66e493018fb980d6ea9
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-08 13:00:59 +0700

    Add tenant testing

--- src/main/java/com/progrema/mkos/repositories/TenantRepository.java
+++ src/main/java/com/progrema/mkos/repositories/TenantRepository.java
@@ -3,5 +3,10 @@ package com.progrema.mkos.repositories;
 import com.progrema.mkos.entities.db.Tenant;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface TenantRepository extends PagingAndSortingRepository<Tenant, Long> {
+
+    List<Tenant> findByTenantName(String name);
+
 }
---
+++ src/test/java/com/progrema/mkos/RepositoryTenantTests.java
@@ -0,0 +1,52 @@
+package com.progrema.mkos;
+
+import com.progrema.mkos.entities.db.Tenant;
+import com.progrema.mkos.repositories.TenantRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RepositoryTenantTests {
+
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @BeforeEach
+    void init() {
+        tenantRepository.deleteAll();
+    }
+
+    @Test
+    void clear() {
+        // do nothing
+    }
+
+    @Test
+    void createTenantTest01() {
+        {
+            Tenant tenant1 = new Tenant();
+            tenant1.setTenantName("M Lutfi");
+            tenant1.setTenantPhone("081811112222");
+            tenantRepository.save(tenant1);
+
+            Tenant tenant2 = new Tenant();
+            tenant2.setTenantName("Ansor Azari");
+            tenant2.setTenantPhone("0818888999");
+            tenantRepository.save(tenant2);
+        }
+        {
+            Tenant tenant1 = tenantRepository.findByTenantName("M Lutfi").get(0);
+            assertEquals("M Lutfi", tenant1.getTenantName());
+            assertEquals("081811112222", tenant1.getTenantPhone());
+
+            Tenant tenant2 = tenantRepository.findByTenantName("Ansor Azari").get(0);
+            assertEquals("Ansor Azari", tenant2.getTenantName());
+            assertEquals("0818888999", tenant2.getTenantPhone());
+        }
+    }
+
+}
