94c0d76158869d346c105341d1333fc3d0285f19
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-08 14:17:28 +0700

    Add tenantLog testing

--- src/test/java/com/progrema/mkos/RepositoryTenantLogTests.java
+++ src/test/java/com/progrema/mkos/RepositoryTenantLogTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -43,6 +45,42 @@ public class RepositoryTenantLogTests {
             assertEquals("Iqbal Pakeh", tenantLog.getTenantName());
             assertEquals("081808488442", tenantLog.getTenantPhone());
             assertEquals("C", tenantLog.getRoomNumber());
+        }
+    }
+
+    @Test
+    void createTenantLogTest02() {
+        {
+            TenantLog tenantLog1 = new TenantLog();
+            tenantLog1.setCheckin(202001L);
+            tenantLog1.setCheckout(202002L);
+            tenantLog1.setTenantName("Iqbal Pakeh");
+            tenantLog1.setTenantPhone("081808488442");
+            tenantLog1.setRoomNumber("C");
+            tenantLogRepository.save(tenantLog1);
+
+            TenantLog tenantLog2 = new TenantLog();
+            tenantLog2.setCheckin(202001L);
+            tenantLog2.setCheckout(202002L);
+            tenantLog2.setTenantName("Iqbal Pakeh");
+            tenantLog2.setTenantPhone("081808488442");
+            tenantLog2.setRoomNumber("C");
+            tenantLogRepository.save(tenantLog2);
+
+            TenantLog tenantLog3 = new TenantLog();
+            tenantLog3.setCheckin(202002L);
+            tenantLog3.setCheckout(202002L);
+            tenantLog3.setTenantName("Iqbal Pakeh");
+            tenantLog3.setTenantPhone("081808488442");
+            tenantLog3.setRoomNumber("C");
+            tenantLogRepository.save(tenantLog3);
+        }
+        {
+            List<TenantLog> tenantLogs202001 = tenantLogRepository.findByCheckin(202001L);
+            assertEquals(2, tenantLogs202001.size());
+
+            List<TenantLog> tenantLogs202002 = tenantLogRepository.findByCheckin(202002L);
+            assertEquals(1, tenantLogs202002.size());
         }
     }
 
