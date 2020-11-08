ba020e94a448b5fb3323e7ff4268915dba12d6cf
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-08 14:48:06 +0700

    Add Room testing

--- src/main/java/com/progrema/mkos/repositories/RoomRepository.java
+++ src/main/java/com/progrema/mkos/repositories/RoomRepository.java
@@ -3,5 +3,10 @@ package com.progrema.mkos.repositories;
 import com.progrema.mkos.entities.db.Room;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends PagingAndSortingRepository<Room, Long> {
+
+    List<Room> findByRoomNumber(String roomNUmber);
+
 }
---
+++ src/test/java/com/progrema/mkos/RepositoryRoomTests.java
@@ -0,0 +1,93 @@
+package com.progrema.mkos;
+import com.progrema.mkos.entities.db.Room;
+import com.progrema.mkos.entities.db.Tenant;
+import com.progrema.mkos.repositories.RoomRepository;
+import com.progrema.mkos.repositories.TenantRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RepositoryRoomTests {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @BeforeEach
+    void init() {
+        roomRepository.deleteAll();
+        tenantRepository.deleteAll();
+    }
+
+    @Test
+    void clear() {
+        // do nothing
+    }
+
+    @Test
+    void testCreateRoomTest01() {
+        {
+            Tenant tenant = new Tenant();
+            tenant.setTenantName("M Lutfi");
+            tenant.setTenantPhone("081811112222");
+            tenantRepository.save(tenant);
+        }
+        {
+            Tenant tenant = tenantRepository.findByTenantName("M Lutfi").get(0);
+            Room room = new Room();
+            room.setRoomNumber("B");
+            room.setRoomRate(550000L);
+            room.setRoomInformation("Located at 2nd floor");
+            room.setTenant(tenant);
+            roomRepository.save(room);
+        }
+        {
+            Room room = roomRepository.findByRoomNumber("B").get(0);
+            assertEquals("B", room.getRoomNumber());
+            assertEquals(550000L, room.getRoomRate());
+            assertEquals("Located at 2nd floor", room.getRoomInformation());
+
+            Tenant tenant = room.getTenant();
+            assertEquals("M Lutfi", tenant.getTenantName());
+            assertEquals("081811112222", tenant.getTenantPhone());
+        }
+    }
+
+    @Test
+    void testCreateRoomTest02() {
+        {
+            Tenant tenant = new Tenant();
+            tenant.setTenantName("M Lutfi");
+            tenant.setTenantPhone("081811112222");
+            tenantRepository.save(tenant);
+        }
+        {
+            Tenant tenant = tenantRepository.findByTenantName("M Lutfi").get(0);
+            Room room = new Room();
+            room.setRoomNumber("B");
+            room.setRoomRate(550000L);
+            room.setRoomInformation("Located at 2nd floor");
+            room.setTenant(tenant);
+            roomRepository.save(room);
+        }
+        {
+            Tenant tenant = tenantRepository.findByTenantName("M Lutfi").get(0);
+            assertEquals("M Lutfi", tenant.getTenantName());
+            assertEquals("081811112222", tenant.getTenantPhone());
+
+            Room room = tenant.getRoom();
+            assertEquals("B", room.getRoomNumber());
+            assertEquals(550000L, room.getRoomRate());
+            assertEquals("Located at 2nd floor", room.getRoomInformation());
+        }
+    }
+
+}
