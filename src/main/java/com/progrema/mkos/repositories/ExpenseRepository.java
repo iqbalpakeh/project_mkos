2ee0ba4843217ef168831f6d230032d1f57a6943
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-08 10:58:45 +0700

    Add expense testing

--- src/main/java/com/progrema/mkos/repositories/ExpenseRepository.java
+++ src/main/java/com/progrema/mkos/repositories/ExpenseRepository.java
@@ -3,5 +3,10 @@ package com.progrema.mkos.repositories;
 import com.progrema.mkos.entities.db.Expense;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long> {
+
+    List<Expense> findByExpenseType(String expenseType);
+
 }
--- src/test/java/com/progrema/mkos/RepositoryExpenseTests.java
+++ src/test/java/com/progrema/mkos/RepositoryExpenseTests.java
@@ -2,9 +2,12 @@ package com.progrema.mkos;
 
 import com.progrema.mkos.entities.db.Expense;
 import com.progrema.mkos.repositories.ExpenseRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class RepositoryExpenseTests {
@@ -12,13 +15,38 @@ class RepositoryExpenseTests {
 	@Autowired
 	private ExpenseRepository expenseRepository;
 
+	@BeforeEach
+	void init() {
+		expenseRepository.deleteAll();
+	}
+
+	@Test
+	void clear() {
+		// do nothing
+	}
+
 	@Test
 	void testCreateExpense01() {
-		Expense expense = new Expense();
-		expense.setExpenseType("Water");
-		expense.setExpenseInformation("Rekening : 12344567");
+		{
+			Expense expense1 = new Expense();
+			expense1.setExpenseType("Water");
+			expense1.setExpenseInformation("Rekening : 12344567");
+			expenseRepository.save(expense1);
 
-		expenseRepository.save(expense);
+			Expense expense2 = new Expense();
+			expense2.setExpenseType("Electricity");
+			expense2.setExpenseInformation("Rekening : 567890");
+			expenseRepository.save(expense2);
+		}
+		{
+			Expense expense1 = expenseRepository.findByExpenseType("Water").get(0);
+			assertEquals(expense1.getExpenseType(), "Water");
+			assertEquals(expense1.getExpenseInformation(), "Rekening : 12344567");
+
+			Expense expense2 = expenseRepository.findByExpenseType("Electricity").get(0);
+			assertEquals(expense2.getExpenseType(), "Electricity");
+			assertEquals(expense2.getExpenseInformation(), "Rekening : 567890");
+		}
 	}
 
 }
