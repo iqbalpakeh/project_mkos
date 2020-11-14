eb8c4fced284bab6e638ddb3f0ee62f99a21f19b
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-14 23:30:40 +0700

    make sure api can be accessed after login

--- src/main/react/src/js/App.js
+++ src/main/react/src/js/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./sections/Login";
 
 class App extends Component {
+	componentDidMount() {
+		axios({
+			method: "GET",
+			url: `/api/tenants`,
+		}).then(
+			(response) => {
+				console.log(response.data);
+			},
+			(error) => console.log(error)
+		);
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>Hello from React</h1>
+				<h1>Hello from React v3</h1>
 				<form action="/logout" method="post">
 					<input type="submit" value="Sign Out"></input>
 				</form>
