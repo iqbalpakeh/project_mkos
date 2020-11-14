61563b2054d298c6b58412a1d8b9f976e5c25839
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-14 19:45:19 +0700

    disable csrf temporarily

--- src/main/java/com/progrema/mkos/configuration/WebSecurityConfig.java
+++ src/main/java/com/progrema/mkos/configuration/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +16,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        // http.authorizeRequests()
+        //         .antMatchers("/", "/home").permitAll()
+        //         .anyRequest().authenticated()
+        //         .and()
+        //         .formLogin()
+        //         .loginPage("/login")
+        //         .permitAll()
+        //         .and()
+        //         .logout()
+        //         .permitAll();
+
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
