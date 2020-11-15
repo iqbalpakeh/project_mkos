aacb6acd91f1e6789e6ce9ab252e5a94beda4843
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-15 11:47:46 +0700

    Change header to const instead of class

--- src/main/react/src/js/components/Header.js
+++ src/main/react/src/js/components/Header.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 
-class Header extends Component {
-	render() {
-		return (
-			<header id="main-header" className="py-2 bg-primary text-white mb-4">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-6">
-							<h1>
-								<i className={this.props.icon}> {this.props.title}</i>
-							</h1>
-						</div>
+const Header = ({ icon, title }) => {
+	return (
+		<header id="main-header" className="py-2 bg-primary text-white mb-4">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<h1>
+							<i className={icon}> {title}</i>
+						</h1>
 					</div>
 				</div>
-			</header>
-		);
-	}
-}
+			</div>
+		</header>
+	);
+};
 
 export default Header;
