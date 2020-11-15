e73cfd9da63a0e595ae4438335346401423fff43
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-15 11:59:24 +0700

    Start to add api call

---
+++ src/main/react/src/js/Api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const fetchIncomePayments = ({ year, startMonth, endMonth }) => {
+	console.log({
+		year,
+		startMonth,
+		endMonth,
+	});
+};
+
+export const fetchExpensePayments = ({ year, startMonth, endMonth }) => {
+	console.log({
+		year,
+		startMonth,
+		endMonth,
+	});
+};
--- src/main/react/src/js/sections/home/index.js
+++ src/main/react/src/js/sections/home/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
+import { fetchIncomePayments, fetchExpensePayments } from "../../Api";
 import Header from "../../components/Header";
 
 class Index extends Component {
+	componentDidMount() {
+		fetchIncomePayments({
+			year: "2020",
+			startMonth: "09",
+			endMonth: "11",
+		});
+
+		fetchExpensePayments({
+			year: "2020",
+			startMonth: "09",
+			endMonth: "11",
+		});
+	}
+
 	render() {
 		return (
 			<div>
