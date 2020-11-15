84cf0481a6f426bd0984e22ea59c0edf3ab84e21
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-15 11:45:21 +0700

    Add components

---
+++ src/main/react/src/js/components/DateFormatter.js
@@ -0,0 +1,62 @@
+export const getYear = (timestamp) => {
+	return timestamp.toString().substring(0, 4);
+};
+
+export const getMonth = (timestamp) => {
+	const month = timestamp.toString().substring(4, 6);
+	switch (month) {
+		case "01":
+			return "January";
+		case "02":
+			return "February";
+		case "03":
+			return "March";
+		case "04":
+			return "April";
+		case "05":
+			return "May";
+		case "06":
+			return "June";
+		case "07":
+			return "July";
+		case "08":
+			return "August";
+		case "09":
+			return "September";
+		case "10":
+			return "October";
+		case "11":
+			return "November";
+		case "12":
+			return "December";
+	}
+};
+
+export const getMonthNumber = (month) => {
+	switch (month) {
+		case "January":
+			return "01";
+		case "February":
+			return "02";
+		case "March":
+			return "03";
+		case "April":
+			return "04";
+		case "May":
+			return "05";
+		case "June":
+			return "06";
+		case "July":
+			return "07";
+		case "August":
+			return "08";
+		case "September":
+			return "09";
+		case "October":
+			return "10";
+		case "November":
+			return "11";
+		case "December":
+			return "12";
+	}
+};
---
+++ src/main/react/src/js/components/Table.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import Pagination from "./Pagination";
+
+class Table extends Component {
+	render() {
+		const { cardHeader, tableHeader, tableEntry } = this.props;
+		return (
+			<section className="mb-4">
+				<div className="container">
+					<div className="row">
+						<div className="col">
+							<div className="card">
+								<div className="card-header">
+									<h4>{cardHeader}</h4>
+								</div>
+								<table className="table table-striped">
+									<thead className="thead-dark">
+										<tr>
+											{tableHeader.map((column, index) => {
+												return <th key={index}>{column}</th>;
+											})}
+										</tr>
+									</thead>
+									<tbody>
+										{tableEntry.map((entry, index) => {
+											return (
+												<tr key={index}>
+													{entry.map((data, index) => {
+														return <td key={index}>{data}</td>;
+													})}
+												</tr>
+											);
+										})}
+									</tbody>
+								</table>
+								{/*<Pagination />*/}
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Table;
