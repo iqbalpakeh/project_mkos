012fa9fccb3fb3da2b35fb52eb841a0fc6306b1d
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-17 22:05:56 +0700

    Build expense header

--- src/main/react/src/js/sections/home/Revenue.js
+++ src/main/react/src/js/sections/home/Revenue.js
@@ -16,12 +16,13 @@ class Revenue extends Component {
 		);
 	}
 
-	buildTableInput({ expense, expenseType }) {
-		if (expense.expenses.length > 0) {
+	buildTableInput({ revenue, room }) {
+		console.log(revenue);
+		if (revenue.revenues.length > 0) {
 			return {
-				cardHeader: this.buildCardHeader(expense),
-				tableHeader: this.buildTableHeader(expense),
-				tableEntry: this.buildTableEntry(expense, expenseType),
+				cardHeader: this.buildCardHeader(revenue),
+				tableHeader: this.buildTableHeader(revenue),
+				tableEntry: this.buildTableEntry(revenue, room),
 			};
 		} else {
 			return {
@@ -32,12 +33,16 @@ class Revenue extends Component {
 		}
 	}
 
-	buildCardHeader(expense) {
-		return `${getYear(expense.expenses[0].timestamp)} Monthly Revenue`;
+	buildCardHeader({ revenues }) {
+		return `${getYear(revenues[0].timestamp)} Monthly Revenue`;
 	}
 
-	buildTableHeader(expense) {
-		return [];
+	buildTableHeader({ revenues }) {
+		const tableHeader = ["Rooms"];
+		revenues.map((element) => {
+			tableHeader.push(getMonth(element.timestamp));
+		});
+		return tableHeader;
 	}
 
 	buildTableEntry(expense, expenseType) {
