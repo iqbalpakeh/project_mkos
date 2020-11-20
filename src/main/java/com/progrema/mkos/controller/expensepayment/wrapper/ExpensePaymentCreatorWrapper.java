c39f3970f81cd9bafc75d13d5d01a0c0cbf6cd7b
Progrema Studio <iqbalpakeh@gmail.com>
2020-11-20 16:12:27 +0700

    Create ExpensePaymentCreatorWrapper

--- src/main/java/com/progrema/mkos/controller/expensepayment/wrapper/ExpensePaymentCreatorWrapper.java
+++ src/main/java/com/progrema/mkos/controller/expensepayment/wrapper/ExpensePaymentCreatorWrapper.java
@@ -1,4 +1,27 @@
 package com.progrema.mkos.controller.expensepayment.wrapper;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ExpensePaymentCreatorWrapper {
+
+    private Long paymentTimestamp;
+
+    private Long paymentAmount;
+
+    private String paymentInformation;
+
+    private String expenseType;
+
+    @Override
+    public String toString() {
+        return "ExpensePaymentCreatorWrapper{" +
+                "paymentTimestamp=" + paymentTimestamp +
+                ", paymentAmount=" + paymentAmount +
+                ", paymentInformation='" + paymentInformation + '\'' +
+                ", expenseType='" + expenseType + '\'' +
+                '}';
+    }
 }
