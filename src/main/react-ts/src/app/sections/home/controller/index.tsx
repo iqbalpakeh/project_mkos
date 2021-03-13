63082f3c09fbb911146ff41f831bc4e3ab75d6ec
Progrema Studio <iqbalpakeh@gmail.com>
2021-03-13 21:56:05 +0700

    prepare adding controller

---
+++ src/main/react-ts/src/app/sections/home/controller/index.tsx
@@ -0,0 +1,5 @@
+const Component = () => {
+	return <div>Controller</div>;
+};
+
+export default Component;
--- src/main/react-ts/src/app/sections/home/index.tsx
+++ src/main/react-ts/src/app/sections/home/index.tsx
@@ -1,9 +1,12 @@
 import { Header } from "app/reusable-components";
 
+import { default as Controller } from "./controller";
+
 const Component = () => {
 	return (
 		<div>
 			<Header icon="fas fa-home" title="Home" />
+			<Controller />
 		</div>
 	);
 };
