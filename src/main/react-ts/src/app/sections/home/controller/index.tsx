9bdb546b78b8eaeeda4e7ef5381cafe86635ceed
Progrema Studio <iqbalpakeh@gmail.com>
2021-03-29 06:30:08 +0700

    Add addpayment button

--- src/main/react-ts/src/app/sections/home/controller/index.tsx
+++ src/main/react-ts/src/app/sections/home/controller/index.tsx
@@ -1,7 +1,20 @@
 import { default as AddPayment } from "./add-payment";
 
 const Component = () => {
-	return <AddPayment />;
+	return (
+		<div>
+			<div className="py-4 mb-2">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-3 mb-2">
+							<AddPayment />
+						</div>
+						<div className="col-md-3">Set Filter</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Component;
